5b72976e16e20163ff81c2b6ee3fff79a0669ac1
asifmahmudumd <mahmu037@d.umn.edu>
2020-08-03 02:09:59 -0500

    SERVER: default tetres windows configuration file added - tetres filters folder created by default

--- Server/src/global_settings.py
+++ Server/src/global_settings.py
@@ -10,10 +10,12 @@ LIB_PATH = os.path.join(os.path.dirname(CUR_PATH), 'libs')
 CONFIG_FILE_PATH = os.path.join(os.path.dirname(CUR_PATH), 'tetres.conf')
 if not os.path.exists(CONFIG_FILE_PATH):
 	CONFIG_FILE_PATH = os.path.join(os.path.dirname('/etc/tetres/'), 'tetres.conf')
+	if not os.path.exists(CONFIG_FILE_PATH):
+		CONFIG_FILE_PATH = 'tetres_default_windows.conf'
 
 # add path
 sys.path.append(LIB_PATH)
 
-DOWNLOAD_TRAFFIC_DATA_FILES = True
+DOWNLOAD_TRAFFIC_DATA_FILES = False
 
 
